ffe67925cda9630c561c45aa62b93cf88acd5d6f
jperraud <jonathan.perraudin@hotmail.com>
2024-12-16 13:36:10 +0100

    tested version

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# Start the dbus-daemon and set the DBUS_SYSTEM_BUS_ADDRESS environment variable
-export DBUS_SYSTEM_BUS_ADDRESS=$(dbus-daemon --fork --config-file=/usr/share/dbus-1/session.conf --print-address)
+# Start the dbus-daemon and set the `DBUS_SESSION_BUS_ADDRESS`` environment variable
 export DBUS_SESSION_BUS_ADDRESS=$(dbus-daemon --fork --config-file=/usr/share/dbus-1/session.conf --print-address)
 
 # Start audio server
